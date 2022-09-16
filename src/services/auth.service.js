d787c5874e2bd8c9e1d8c2249a48544ba2ed1d4b
Hank <hangryan@gmail.com>
2022-09-16 16:37:58 -0400

    change host

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+// const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://exeltechcontactformbackend.herokuapp.com/api/auth/";
 
 
 
