e341745c5017fc8906c9e904a9c10f0e40202f29
Hank <hangryan@gmail.com>
2022-12-05 10:50:33 -0500

    correction

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:8080/api",
   // baseURL: "https://exeltechdatabase.herokuapp.com/api",
-  baseURL: "https://long-jade-crocodile-coat.cyclic.app",
+  baseURL: "https://panicky-erin-mittens.cyclic.app",
   headers: {
     "Content-type": "application/json"
   }
